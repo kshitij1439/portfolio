23e790f4bdfd6bc5e4046b3be337bda9ac22c245
kshitij1439 <kshitijgaikwad142004@gmail.com>
2024-12-24 18:59:47 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2024 Kshitij's Portfolio. All rights reserved.</p>
+      <p>© 2024 KG Portfolio. All rights reserved.</p>
     </footer>
   );
 }
