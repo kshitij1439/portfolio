294f082cee0ad8cd5170cd17ab51e15eb676b82b
kshitij1439 <kshitijgaikwad142004@gmail.com>
2025-12-09 21:53:19 +0530

    rf:  updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import './App.css'
-import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
+import Hero from "./components/About";
 import Skills from "./components/Skills";
-import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Working from "./components/Working";
 import Footer from "./components/Footer";
-import './styles/tailwind.css';  
-// import Working from './components/Working.jsx';
-function App() {
+import Contact from "./components/Contact";
 
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      {/* <Working/> */}
-      <Contact />
-      <Footer />
-    </div>
-  )
+function App() {
+    return (
+        <div className="bg-slate-950 min-h-screen text-slate-200 antialiased selection:bg-purple-500/30">
+            <Navbar />
+
+            <main>
+                <Hero />
+                <Projects />
+                <Working />
+                <Skills />
+                <Contact />
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
